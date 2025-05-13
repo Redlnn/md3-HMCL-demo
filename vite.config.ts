3c0625baaa7526847449faa8763ca6f220dc6bd7
Redlnn <w731347477@gmail.com>
2025-05-13 22:36:09 +0800

    配置mdui

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // 所有以 mdui- 开头的标签名都是 mdui 组件
+          isCustomElement: (tag) => tag.startsWith('mdui-'),
+        },
+      },
+    }),
+    vueJsx(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
